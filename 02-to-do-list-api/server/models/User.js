fddf7db5f7f5128aa6c53f68af9cfc1518d5455b
MaxGitHub0515 <105605213+MaxGitHub0515@users.noreply.github.com>
2024-10-20 20:20:50 +0200

    bcrypt schema upd

--- 02-to-do-list-api/server/models/User.js
+++ 02-to-do-list-api/server/models/User.js
@@ -29,13 +29,15 @@ const UserSchema =  new mongoose.Schema({
 })
 
 
-
 UserSchema.pre('save', async () => {
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
 } )
 
-UserSchema.methods.com
+UserSchema.methods.comparePassword = async function (candidatePassword){
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch
+}
 
 
 
