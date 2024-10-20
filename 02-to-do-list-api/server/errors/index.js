0a3ea0a63b437a37e79758e6868228c21bd18e04
MaxGitHub0515 <105605213+MaxGitHub0515@users.noreply.github.com>
2024-10-20 22:16:34 +0200

    upd errors

--- 02-to-do-list-api/server/errors/index.js
+++ 02-to-do-list-api/server/errors/index.js
@@ -2,12 +2,13 @@
 const {CustomAPIError} = require('./custom-error');
 const NotFoundError = require('./not-found');
 const BadRequestError = require('./bad-request');
-
+const UnauthenticatedError = require('./unauthenticated')
 
 module.exports = {
     CustomAPIError,
     NotFoundError,
-    BadRequestError
+    BadRequestError,
+    UnauthenticatedError
 
 }
 
