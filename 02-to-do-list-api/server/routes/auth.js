da77ef61f01ffff59e450dfe0768450a7b625051
MaxGitHub0515 <105605213+MaxGitHub0515@users.noreply.github.com>
2024-11-02 19:29:22 +0100

    auth route added

---
+++ 02-to-do-list-api/server/routes/auth.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+
+const router = express.Router();
+
+const {
+   
+
+   
+} = require('../controllers/');
+
+router.route('/').get().post();
+router.route()
+
+
+
+module.exports = router;
